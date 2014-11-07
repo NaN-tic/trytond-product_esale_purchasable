aad747e7e335464f5cef347ecc9cfbb3af44114c
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:29:26 +0100

    Cleanup tests

--- tests/test_product_esale_purchasable.py
+++ tests/test_product_esale_purchasable.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python
-#This file is part product_esale_purchasable module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part product_esale_purchasable module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 from trytond.transaction import Transaction
 
 class ProducteSalePurchasableTestCase(unittest.TestCase):
-    '''
-    Test Product eSale Purchasable module.
-    '''
+    'Test Product eSale Purchasable module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('product_esale_purchasable')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('product_esale_purchasable')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
